dea00d530fd737b4f2494b37c71795d3aeef73e2
ravenera0317 <r6090408@gmail.com>
2021-10-01 18:19:04 +0800

    Initialize project

---
+++ README.md
@@ -0,0 +1,45 @@
+# Random Password Generator
+
+一個簡單的隨機密碼產生器
+
+## Preview
+
+![](preview.png)
+
+## Prerequisites
+
+- Body-Parser v1.19.0
+- Express v4.17.1
+- Express-Handlebars v5.3.2
+
+## Installing
+
+1. 打開終端機 (Terminal) 輸入以下訊息，下載專案至本機
+
+```
+git clone https://github.com/ravenera0317/password-generator.git
+```
+
+2. 進入專案目錄資料夾
+
+```
+cd password-generator
+```
+
+3. 安裝 npm 套件
+
+```
+npm install
+```
+
+4. 啟動伺服器來執行專案
+
+```
+nodemon app.js
+```
+
+5. 出現以下訊息即可打開瀏覽器輸入 http://localhost:3000 開始使用
+
+```
+Express app listening on port 3000.
+```
---
+++ app.js
@@ -0,0 +1,28 @@
+// Include packages and define server related variables
+const express = require('express')
+const exphbs = require('express-handlebars')
+const generatePassword = require('./generate_password')
+const app = express()
+const port = 3000
+
+// setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// setting body-parser
+app.use(express.urlencoded({ extended: true }))
+
+// setting routes
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.post('/', (req, res) => {
+  const options = req.body
+  const password = generatePassword(options)
+  res.render('index', { password, options })
+})
+
+app.listen(port, () => {
+  console.log(`Express app listening on port ${port}.`)
+})
